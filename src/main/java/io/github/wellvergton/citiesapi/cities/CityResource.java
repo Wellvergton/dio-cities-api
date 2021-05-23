bd0f9ff46c549fbdc381612385dd70eecd9b0294
Wellvergton <wmc.jp@outlook.com>
2021-05-22 23:49:23 -0300

    aula 10

---
+++ src/main/java/io/github/wellvergton/citiesapi/cities/CityResource.java
@@ -0,0 +1,26 @@
+package io.github.wellvergton.citiesapi.cities;
+
+import io.github.wellvergton.citiesapi.cities.City;
+import io.github.wellvergton.citiesapi.cities.CityRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cities")
+public class CityResource {
+
+	private final CityRepository repository;
+
+	public CityResource(final CityRepository repository) {
+		this.repository = repository;
+	}
+
+	@GetMapping
+	public Page<City> cities(final Pageable page) {
+		return repository.findAll(page);
+	}
+
+}
